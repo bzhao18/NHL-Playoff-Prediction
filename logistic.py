29980f4ecedae7bc7cc06bedaa5b56810ac2ef3e
Gabby <ggermanson@gatech.edu>
2021-10-30 14:13:43 -0400

    Logistic regression implementation

--- logistic.py
+++ logistic.py
@@ -1,80 +1,70 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt 
-import seaborn as sns
 import statsmodels.api as sm
-from sklearn import preprocessing
-from sklearn import metrics
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 
-# Get Numerical Data 
-# Skip team_id
+# TODO: Pre-process data (feature selection like PCA) and then use this script
+
+# Get numerical data from game_stats.csv. Skip team_id.
+# goalie_stats.csv and skater_stats.csv data not used yet since need to join with game_stats to have a 'won' label
 game_data_columns = ['game_id', 'won', 'goals', 'shots', 'pim', 'powerPlayOpportunities', 'injured_player_count']
-
 game_data = pd.read_csv("cleaned_data/game_stats.csv", usecols=game_data_columns)
 # Change 'won' column from boolean to int type
 game_data['won'] = game_data['won'].astype(int)
-# Blanks in 'injured_player_count' changed from NaN type to 0
+# Change NaN in 'injured_player_count' column to 0
 game_data['injured_player_count'] = game_data['injured_player_count'].fillna(0)
 
-goalie_data_columns = ['game_id', 'team_id', 'player_id', 'timeOnIce', 'shots', 'saves', 'savePercentage']
-goalie_data = pd.read_csv("cleaned_data/goalie_stats.csv", usecols=goalie_data_columns)
-goalie_data['savePercentage'] = goalie_data['savePercentage'].fillna(0)
+# TODO: Split data into training and testing data by first and second half of the season.
+X = game_data.loc[:, game_data.columns != 'won'] # Data
+y = game_data.loc[:, game_data.columns == 'won'] # Data's labels
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-skater_data_columns = ['game_id', 'team_id', 'player_id', 'timeOnIce', 'assists', 'goals', 'shots','penaltyMinutes']
-skater_data = pd.read_csv("cleaned_data/skater_stats.csv", usecols=skater_data_columns)
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# Logistic Regression implementation follows https://towardsdatascience.com/building-a-logistic-regression-in-python-step-by-step-becd4d56c9c8
 
-# -------------------------------------------------------------------------------------------------------------- #
-# Following https://towardsdatascience.com/building-a-logistic-regression-in-python-step-by-step-becd4d56c9c8
+# Info on current balance of win and loss rows
+rows = X.shape[0]
+win_rows = y[y['won'] == 1].shape[0]
+loss_rows = y[y['won'] == 0].shape[0]
+print("Rows: {}. Win rows: {}. Loss rows: {}".format(rows, win_rows, loss_rows))
 
-plt.rc("font", size=14)
+# Balance data to have equal win and loss rows
+oversampled = SMOTE(random_state=0)
+oversampled_X, oversampled_y = oversampled.fit_resample(X_train, y_train)
+X = pd.DataFrame(data=oversampled_X, columns=X_train.columns)
+y = pd.DataFrame(data=oversampled_y, columns=['won'])
+# Info on currentbalance of win and loss rows
+rows = X.shape[0]
+win_rows = y[y['won'] == 1].shape[0]
+loss_rows = y[y['won'] == 0].shape[0]
+print("Rows: {}. Win rows: {}. Loss rows: {}".format(rows, win_rows, loss_rows))
 
-sns.set(style="white")
-sns.set(style="whitegrid", color_codes=True)
+# Above link used Recursive Feature Elimination for data pre-processing at this point.
 
-# Variable to Predict: 'won' from game_data
-# Print info on training data 'won' labels
-print(game_data['won'].value_counts())
-sns.countplot(x='won',data=game_data, palette='hls')
-# plt.show()
+# Model implementation
+logit_model = sm.Logit(y, X)
+result = logit_model.fit()
+print(result.summary2())
 
-losses = len(game_data[game_data['won']==0])
-wins = len(game_data[game_data['won']==1])
-pct_loss = losses/(losses+wins)
-print("Percentage of losses", pct_loss*100)
-pct_wins = wins/(losses+wins)
-print("Percentage of wins", pct_wins*100)
+# Link removed any insignificant feature having p-value >= .05 at this point (P>|z| column)
+# Note: game_data features are all significant (game_stats.csv version on 10/30/21)
 
-X = game_data.loc[:, game_data.columns != 'won']
-y = game_data.loc[:, game_data.columns == 'won']
-
-os = SMOTE(random_state=0)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-columns = X_train.columns
-os_data_X,os_data_y=os.fit_resample(X_train, y_train)
-os_data_X = pd.DataFrame(data=os_data_X,columns=columns)
-os_data_y= pd.DataFrame(data=os_data_y,columns=['won'])
-# we can Check the numbers of our data
-print("length of oversampled data is ",len(os_data_X))
-print("Number of losses in oversampled data",len(os_data_y[os_data_y['won']==0]))
-print("Number of wins",len(os_data_y[os_data_y['won']==1]))
-print("Proportion of loss data in oversampled data is ",len(os_data_y[os_data_y['won']==0])/len(os_data_X))
-print("Proportion of win data in oversampled data is ",len(os_data_y[os_data_y['won']==1])/len(os_data_X))
-
-# Could perform Recursive Feature Elimination here
-
-# Logistic Regression Model
-# Notes: Running on just on game_data and all features are significant.
-X = os_data_X
-y= os_data_y
-logit_model=sm.Logit(y,X)
-result=logit_model.fit()
-# print(result.summary2())
-
-# Remove any features that have p-values greater or equal to .05 here (look at P>|z| column)
-X_train, X_test, y_train, y_test = train_test_split(X, np.ravel(y), test_size=0.3, random_state=0)
+# Logistic Regression Classifier Model Fitting
 logreg = LogisticRegression()
-logreg.fit(X_train, y_train)
+logreg.fit(X_train, np.ravel(y_train))
+
+# Predict test data labels
+y_pred = logreg.predict(X_test)
+score = logreg.score(X_test, y_test)
+print('Accuracy on test data: {:.2f}'.format(score))
+
+print('Confusion matrix:\n', confusion_matrix(y_test, y_pred))
+
+# Link calculates precision, recall, F-measure, and support at this point
+
+# Link plots receiver operating characteristic (ROC) curve at this point
+
+# TODO: Utilize y_pred to determine predicted playoff bracket rankings
