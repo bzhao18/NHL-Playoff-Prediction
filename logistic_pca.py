0981f79c4afd86e81f06190402c116694d56461e
Gabby <ggermanson@gatech.edu>
2021-11-08 17:49:27 -0500

    Seasons test/train data split

--- logistic_pca.py
+++ logistic_pca.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import math
+import random
 import statsmodels as sm
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, roc_curve, auc
@@ -9,10 +11,24 @@ from sklearn.preprocessing import StandardScaler
 
 from sklearn.preprocessing import MinMaxScaler
 
-# transform data using PCA and use transformed data for logistic regression
-def logistic_pca(data, target, n_components):
-    # split data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=42)
+# Transform data using PCA and use transformed data for logistic regression
+def logistic_pca(data, n_components):
+    # Sample .2 (round up) number of seasons
+    seasons = set(data['season'])
+    num_seasons = len(seasons)
+    num_test_seasons = math.ceil(num_seasons * .2)
+    test_seasons = random.sample(seasons, num_test_seasons) # Need seed approach if want same sample when run again
+    print("Test seasons are:", test_seasons)
+
+    # Split data into training and test sets
+    X_train = data.loc[~data['season'].isin(test_seasons)]
+    X_test = data.loc[data['season'].isin(test_seasons)]
+    y_train = X_train.loc[:, 'end_season_playoff_standing']
+    y_test = X_test.loc[:, 'end_season_playoff_standing']
+    X_train = X_train.drop('end_season_playoff_standing', 1)
+    X_test = X_test.drop('end_season_playoff_standing', 1)
+    print("Proportion training: {0:.4f} | Proportion testing: {1:.4f}".format(X_train.shape[0] / data.shape[0], X_test.shape[0] / data.shape[0]))
+
     # standardize data
     scaler = StandardScaler()
     scaler.fit(X_train)
@@ -58,10 +74,7 @@ game_data = pd.read_csv("cleaned_data_v3/first_half_season_summary.csv", usecols
 game_data['end_season_playoff_standing'] = game_data['end_season_playoff_standing'].fillna(0)
 game_data['end_season_playoff_standing'] = game_data['end_season_playoff_standing'].astype(int)
 
-# get X and y
-X = game_data.loc[:, game_data.columns != 'end_season_playoff_standing']
-y = game_data.loc[:, game_data.columns == 'end_season_playoff_standing']
-logistic_pca(X, y, 5)
+logistic_pca(game_data, 5)
 
 
 
