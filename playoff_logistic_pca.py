c766ac3f4c114a115538080de5cdc45c0e1b052c
Gabby <ggermanson@gatech.edu>
2021-11-10 12:19:36 -0500

    Playoff version of log_pca.py, not fully developed yet

--- playoff_logistic_pca.py
+++ playoff_logistic_pca.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import random
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 from imblearn.over_sampling import SMOTE
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, roc_curve, auc
@@ -15,7 +16,7 @@ def logistic_pca(data, n_components):
     seasons = set(data['season'])
     num_seasons = len(seasons)
     num_test_seasons = math.ceil(num_seasons * .2)
-    # random.seed(150) # Uncomment to get same sample. Change seed value to get different sample
+    random.seed(150) # Uncomment to get same sample. Change seed value to get different sample
     test_seasons = random.sample(seasons, num_test_seasons)
     print("Test seasons are:", test_seasons)
 
@@ -49,13 +50,17 @@ def logistic_pca(data, n_components):
     X_train = pca.transform(X_train)
     X_test = pca.transform(X_test)
 
-    # Would typically run model implementation that's commented out below to remove insignificant
-    # features of p-value >= .05 (P>|z| column), and then rerun model implementation.
-    # However, this requires the labels to be either 0s or 1s as Logistic Regression requires
-    # import statsmodels.api as sm
-    # logit_model = sm.Logit(y_train, X_train)
-    # result = logit_model.fit()
-    # print(result.summary2())
+    # # Remove insignificant dimensions of p-value >= .05 (P>|z| column).
+    # summary = logit_model_summary(X_train, y_train) # Model implementation
+    # # print(summary)
+    # p_values = summary.tables[1]['P>|z|']
+    # remove_dim_bool, dims_to_remove = check_insignificant(p_values)
+    # while remove_dim_bool:
+    #     X_train = np.delete(X_train, dims_to_remove, axis=1)
+    #     X_test = np.delete(X_test, dims_to_remove, axis=1)
+    #     summary = logit_model_summary(X_train, y_train)
+    #     p_values = summary.tables[1]['P>|z|']
+    #     remove_dim_bool, dims_to_remove = check_insignificant(p_values)
 
     # Logistic Regression
     logreg = LogisticRegression()
@@ -63,7 +68,8 @@ def logistic_pca(data, n_components):
 
     # Predict test data labels
     y_pred = logreg.predict(X_test)
-    print(y_pred)
+    print("Predicted", y_pred)
+    print("Actual", y_test)
     score = logreg.score(X_test, y_test)
     print('Accuracy on test data: {:.2f}'.format(score))
 
@@ -87,29 +93,31 @@ def logistic_pca(data, n_components):
     # See link in logistic.py to implement other measurements like precision, recall, F-measure, and support
 
 def balance_info(X_train, y_train):
-    standing_1_rows = y_train[y_train == 1].shape[0]
-    standing_2_rows = y_train[y_train == 2].shape[0]
-    standing_3_rows = y_train[y_train == 3].shape[0]
-    standing_4_rows = y_train[y_train == 4].shape[0]
-    standing_5_rows = y_train[y_train == 5].shape[0]
-    standing_6_rows = y_train[y_train == 6].shape[0]
-    standing_7_rows = y_train[y_train == 7].shape[0]
-    standing_8_rows = y_train[y_train == 8].shape[0]
-    standing_9_rows = y_train[y_train == 9].shape[0]
-    standing_10_rows = y_train[y_train == 10].shape[0]
-    standing_11_rows = y_train[y_train == 11].shape[0]
-    standing_12_rows = y_train[y_train == 12].shape[0]
-    standing_13_rows = y_train[y_train == 13].shape[0]
-    standing_14_rows = y_train[y_train == 14].shape[0]
-    standing_15_rows = y_train[y_train == 15].shape[0]
-    standing_16_rows = y_train[y_train == 16].shape[0]
+    playoff_rows = y_train[y_train == 1].shape[0]
     no_playoff_rows = y_train[y_train == 0].shape[0]
-    print("Rows: {}. Rank 1: {}. Rank 2: {}. Rank 3: {}. Rank 4: {}. Rank 5: {}. Rank 6: {}. Rank 7: {}. Rank 8: {}. Rank 9: {}. Rank 10: {}. Rank 11: {}. Rank 12: {}. Rank 13: {}. Rank 14: {}. Rank 15: {}. Rank 16: {}. No playoffs: {}. ".format(X_train.shape[0], standing_1_rows, standing_2_rows, standing_3_rows, standing_4_rows, standing_5_rows, standing_6_rows, standing_7_rows, standing_8_rows, standing_9_rows, standing_10_rows, standing_11_rows, standing_12_rows, standing_13_rows, standing_14_rows, standing_15_rows, standing_16_rows, no_playoff_rows))
+    print("Rows: {}. Playoffs: {}. No playoffs: {}. ".format(X_train.shape[0], playoff_rows, no_playoff_rows))
     
+def check_insignificant(p_values):
+    removed_dim_bool = False
+    dims_to_remove = []
+    for index, p in enumerate(p_values):
+        if p >= .05:
+            dims_to_remove.append(index)
+            removed_dim_bool = True
+    print(dims_to_remove)
+    return removed_dim_bool, dims_to_remove
+
+# Model implementation
+def logit_model_summary(X_train, y_train):
+    logit_model = sm.Logit(y_train, X_train)
+    result = logit_model.fit()
+    return result.summary2()
+
 # Read in data
 game_data_columns = ['team_id','season','total_first_half_season_wins','total_first_half_season_shots','total_first_half_season_goals','total_first_half_season_pim','total_first_half_season_powerPlayOpportunities','end_season_playoff_standing']
 game_data = pd.read_csv("cleaned_data_v3/first_half_season_summary.csv", usecols=game_data_columns)
 game_data['end_season_playoff_standing'] = game_data['end_season_playoff_standing'].fillna(0) # Change NaNs to 0
 game_data['end_season_playoff_standing'] = game_data['end_season_playoff_standing'].astype(int)
+game_data['end_season_playoff_standing'] = game_data['end_season_playoff_standing'].mask(game_data['end_season_playoff_standing'] > 0, 1)
 
 logistic_pca(game_data, 5)
