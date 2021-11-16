f5aba27937153d07fcb026776d1e06a545430afe
Wallace, Noah <nwallace9@gatech.edu>
2021-11-16 10:06:48 -0500

    Add files via upload

--- pca.py
+++ pca.py
@@ -14,8 +14,14 @@ game_data_columns = ['team_id', 'game_id', 'won', 'goals', 'shots', 'pim', 'powe
 goalie_data_columns = ['game_id', 'team_id', 'player_id', 'timeOnIce', 'shots', 'saves', 'savePercentage']
 skater_data_columns = ['game_id', 'team_id', 'player_id', 'timeOnIce', 'assists', 'goals', 'shots', 'penaltyMinutes']
 
-first_half_columns = ['team_id', 'season', 'total_first_half_season_wins', 'total_first_half_season_shots', 'total_first_half_season_goals', \
-'total_first_half_season_pim', 'total_first_half_season_powerPlayOpportunities']
+first_half_columns = ['team_id', 'season', 'total_first_half_season_wins', 'total_first_half_season_shots',
+                      'total_first_half_season_goals',
+                      'total_first_half_season_pim', 'total_first_half_season_powerPlayOpportunities']
+# For visualization only
+# This is shortening the names for data visualization -> better legibility
+first_half_columns_modified = ['team_id', 'season_in_2000s', 'first_half_season_wins', 'first_half_season_shots',
+                               'first_half_season_goals',
+                               'first_half_season_pim', 'first_half_season_ppo']
 
 # reading in csv files
 game_data = pd.read_csv("cleaned_data/game_stats.csv", usecols=game_data_columns)
@@ -32,11 +38,17 @@ skater_data = pd.read_csv("cleaned_data/skater_stats.csv", usecols=skater_data_c
 # first_half_season_summary.csv file
 first_half = pd.read_csv("cleaned_data_v3/first_half_season_summary.csv", usecols=first_half_columns)
 
-# The visualize function can also be used on the pre-PCA data. However, this will take a bit to run.
-# v.visualizeComponents(data=np.array(game_data), labels=game_data_columns)
-# v.visualizeComponents(data=np.array(goalie_data), labels=goalie_data_columns)
-# v.visualizeComponents(data=np.array(skater_data), labels=skater_data_columns)
+# For visualization only
+# The names have been shortened in the file and the seasons have been changed from e.g. 2012 to 12 to reduce cluttering
+first_half_modified = pd.read_csv("cleaned_data_v3/first_half_season_summary_modified.csv",
+                                  usecols=first_half_columns_modified)
 
+# The visualize function can also be used on the pre-PCA data. However, this will take a bit to run.
+v.visualizeComponents(data=np.array(game_data), labels=game_data_columns, title='Pre-PCA Game Data')
+v.visualizeComponents(data=np.array(goalie_data), labels=goalie_data_columns, title='Pre-PCA Goalie Data')
+v.visualizeComponents(data=np.array(skater_data), labels=skater_data_columns, title='Pre-PCA Skater Data')
+v.visualizeComponents(data=np.array(first_half_modified), labels=first_half_columns_modified,
+                      title='First Half Season Summary Data')
 # -------------------------------------------------------------------------------------------------------------- #
 ## finding the correct number of components to get for each dataset 
 ## plots graph of variance vs components 
@@ -90,7 +102,7 @@ print(x1.shape)
 
 print(principal1.components_)
 
-# v.visualizeComponents(data=x1)
+v.visualizeComponents(data=x1, title='Post-PCA Game Data')
 
 ## 4 components for goalie_stats.csv
 df2 = pd.DataFrame(game_data)
@@ -105,7 +117,7 @@ print(x2.shape)
 
 print(principal2.components_)
 
-# v.visualizeComponents(data=x2)
+v.visualizeComponents(data=x2, title='Post-PCA Goalie Data')
 
 ## 5 components for skater_stats.csv
 df3 = pd.DataFrame(skater_data)
@@ -120,8 +132,7 @@ print(x3.shape)
 
 print(principal3.components_)
 
-# v.visualizeComponents(data=x3)  # Slow because there are a lot of data points
-
+v.visualizeComponents(data=x3, title='Post-PCA Skater Data')  # Slow because there are a lot of data points
 
 ## 5 components for first_half_season_summary.csv
 df4 = pd.DataFrame(first_half)
@@ -136,4 +147,4 @@ print(x4.shape)
 
 print(principal4.components_)
 
-# v.visualizeComponents(data=x4)  # Slow because there are a lot of data points
+v.visualizeComponents(data=x4, title='Post-PCA First Half Season Data')  # Slow because there are a lot of data points
